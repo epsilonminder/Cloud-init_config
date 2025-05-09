5ba19cd607d9b488ce5814a4c081f18816c8be43
epsilonminder <55226776+epsilonminder@users.noreply.github.com>
2025-05-09 23:33:59 +0900

    Update cloud-init_conf.bash
    set storage size

--- cloud-init_conf.bash
+++ cloud-init_conf.bash
@@ -5,6 +5,7 @@ VMID=9000
 VMNAME="ubuntu24-cloud"
 MEMORY=2048
 STORAGE="working"
+STORAGE_SIZE="10G"
 IMAGE_PATH="/mnt/pve/working/template/iso/noble-server-cloudimg-amd64.img"
 TARGET_STORAGE="local-lvm"
 
@@ -19,6 +20,10 @@ qm importdisk ${VMID} ${IMAGE_PATH} ${STORAGE}
 # === scsi0 に割り当て ===
 echo "ディスクを scsi0 に割り当て"
 qm set ${VMID} --scsihw virtio-scsi-pci --scsi0 ${STORAGE}:vm-${VMID}-disk-0
+
+# === ディスクサイズを拡張 ===
+echo "ディスクサイズを拡張します"
+qm resize ${VMID} scsi0 ${STORAGE_SIZE}
 
 # === VMをテンプレートに変換 ===
 echo "VMをテンプレート化"
